ed1b459920461ca55be2e93c65df4a801ee64dd4
echo1 <coloveyx@gamil.com>
2023-10-09 10:35:42 +0500

    optimized py.py

--- py.py
+++ py.py
@@ -1,16 +1,12 @@
 def sum_of_intervals(intervals):
-    count = 0
     intervals =  sorted(set(intervals))
-    if len(intervals) >= 2:
-        for i in range(len(intervals)-1):
-            if intervals[i][1] >= intervals[i+1][0]:
-                match intervals[i+1][1] >= intervals[i][1]:
-                    case True: intervals[i+1] = (intervals[i][0], intervals[i+1][1])
-                    case _: intervals[i+1] = (intervals[i][0], intervals[i][1])
-                intervals[i] = set()
-        intervals = [i for i in intervals if i != set()]
-        for interval in intervals:
-            count += interval[1] - interval[0]
-    else:
-        return intervals[0][1] - intervals[0][0]
-    return count
+    for i in range(len(intervals)-1):
+        if intervals[i][1] >= intervals[i+1][0]:
+            match intervals[i+1][1] >= intervals[i][1]:
+                case True: intervals[i+1] = (intervals[i][0], intervals[i+1][1])
+                case _: intervals[i+1] = (intervals[i][0], intervals[i][1])
+            intervals[i] = set()
+    intervals = sum(i[1]-i[0] for i in intervals if i != set())
+    return intervals
+
+print(sum_of_intervals([(-10,100)]))
